bb172711138c54e0820033aaf25f5e31e2a5f216
bartschneider <crealdocsnyder@mailbox.org>
2024-06-23 15:57:43 +0300

    keine ahnung

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,7 +16,16 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({
+                settings = {
+                    diagnostics = {
+                        globals = {'vim'}
+                    }
+                }
+            })
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
 }
